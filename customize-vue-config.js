5d2054f399c47a92f388589b699f1ae195d8c190
Hassan Iftikhar <144235442+hassan-iftikhar00@users.noreply.github.com>
2024-07-08 14:03:31 +0500

    Project Push

---
+++ customize-vue-config.js
@@ -0,0 +1,24 @@
+import { theme } from './src/config/theme/themeVariables';
+
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VUE_APP_SUB_ROUTE
+        ? process.env.VUE_APP_SUB_ROUTE
+        : process.env.BASE_URL
+      : process.env.BASE_URL,
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            ...theme,
+          },
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
+};
+console.log("hassan hassan hassan");
+
